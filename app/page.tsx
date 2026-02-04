62ae3735016afa0bdae61d9f2060732e1de8a266
fahadsaeed8 <120630793+fahadsaeed8@users.noreply.github.com>
2026-02-04 11:35:40 +0500

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,6 @@
 "use client";
 
 import { useEffect } from "react";
-import HeroSection from "./components/HeroSection";
-import WhoWeAreSection from "./components/WhoWeAreSection";
-import EasyAndSafeFeaturesSection from "./components/EasyAndSafeFeaturesSection";
-import WhereRealConnectionsSection from "./components/WhereRealConnectionsSection";
-import StatisticsSection from "./components/StatisticsSection";
-import DownloadAppSection from "./components/DownloadAppSection";
-import FooterSection from "./components/FooterSection";
 
 export default function Home() {
   // Ensure page scrolls to top on refresh to show "Enter the Circle" section
@@ -26,13 +19,7 @@ export default function Home() {
 
   return (
     <main className="w-full overflow-x-hidden max-w-full">
-      <HeroSection />
-      <WhoWeAreSection />
-      <EasyAndSafeFeaturesSection />
-      <WhereRealConnectionsSection />
-      <StatisticsSection />
-      <DownloadAppSection />
-      <FooterSection />
+      <p>hellow</p>
     </main>
   );
 }
